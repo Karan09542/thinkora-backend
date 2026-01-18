6daa17cdaa79d1aa761a7cf42ec9b2d29c54a439
Karan09542 <karan09542@gmail.com>
2026-01-18 17:04:26 +0530

    explicit defined cors method

--- src/app.ts
+++ src/app.ts
@@ -13,16 +13,16 @@ const app = express();
 app.use(
   cors({
     origin: config.frontendURL,
-    methods: "*",
+    methods: ["POST", "GET", "DELETE"],
     credentials: true,
-  })
+  }),
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (_req, res) => {
-  res.send("<h1>hariom</h1>")
+  res.send("<h1>hariom</h1>");
 });
 
 app.use("/v1", router);
